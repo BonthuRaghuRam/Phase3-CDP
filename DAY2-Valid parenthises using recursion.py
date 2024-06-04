549206731518497921a97fe8d62875646cce11b0
BonthuRaghuRam <raghubonthu2004@gmail.com>
2024-06-04 10:31:05 +0530

    Update DAY2-Valid parenthises using recursion.py

--- DAY2-Valid parenthises using recursion.py
+++ DAY2-Valid parenthises using recursion.py
@@ -15,3 +15,22 @@ def par(size,res,n,n1,n2):
 
 n=int(input())
 par(0,"",n,0,0)
+
+alternative:
+
+
+def printThis(n, result, openOnes, closedOnes):
+    if closedOnes > openOnes:
+        return
+    if openOnes > (n // 2) or closedOnes > (n // 2):
+        return
+    if len(result) == n:
+        print(result)
+        return 
+ 
+    printThis(n, result + "(", openOnes + 1, closedOnes)
+    printThis(n, result + ")", openOnes, closedOnes + 1)
+ 
+ 
+n = 6
+printThis(n, "", 0, 0)
