7c6305a4f4f072e593d89a3806d36e33494c8254
BonthuRaghuRam <raghubonthu2004@gmail.com>
2024-06-12 18:38:56 +0530

    Create DAY8_BinarySearch.py

---
+++ DAY8_BinarySearch.py
@@ -0,0 +1,31 @@
+# recursive approach
+class Solution(object):
+    def search(self, nums, target):
+
+        def recursiveBinarySearch(left, right):
+            if left > right:
+                return -1 
+            
+            mid = (left + right) // 2 
+            if nums[mid] == target:
+                return mid 
+            elif nums[mid] > target:
+                return recursiveBinarySearch(left, mid - 1)
+            return recursiveBinarySearch(mid + 1, right)
+
+        return recursiveBinarySearch(0, len(nums) - 1)
+
+  # Iterative Approach
+  class Solution(object):
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1 
+        while left <= right:
+            mid = (left + right) // 2 
+            if nums[mid] == target:
+                return mid 
+            elif nums[mid] > target:
+                right = mid - 1 
+            else:
+                left = mid + 1 
+        return -1
+        
